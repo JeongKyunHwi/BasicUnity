9fffddb71feaccf989293981ef2343d7f15b2191
JeongKyunHwi <rbsgnl123@gmail.com>
2025-03-10 17:04:34 +0900

    basic_task

--- Basic1/Assets/Scripts/MoveWithGravity.cs
+++ Basic1/Assets/Scripts/MoveWithGravity.cs
@@ -10,7 +10,7 @@ public class MoveWithGravity : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb.freezeRotation = true;
     }
 
     // Update is called once per frame
